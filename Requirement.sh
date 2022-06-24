ec0e2d7067d2d7d3f3f5d7a66aa6352247b9eccf
WhiteHiru <108093274+WhiteHiru@users.noreply.github.com>
2022-06-24 12:48:26 +0530

    Update Requirement.sh

--- Requirement.sh
+++ Requirement.sh
@@ -8,6 +8,8 @@ p='\e[1;35m'
 lb='\e[1;36m'
 
 clear
+apt update
+apt upgrade
 pkg install figlet
 figlet Basic Installation
 pkg install toilet -y
@@ -15,13 +17,14 @@ pkg install cowsay -y
 pkg install nano -y
 pkg install ruby -y
 pkg install lolcat
+pkg install wget 
 apt update
 apt upgrade
 clear
 figlet -f big FINISHED!! | lolcat
 echo ""
 sleep 3
-echo -e $lb " subscribe my youtube channel "
+echo -e $lb " [+] subscribe my youtube channel "
 sleep 1
 termux-open-url https://www.youtube.com/channel/UCfL8-OWCMDKj0PRzgEA_WXg
 echo ""
